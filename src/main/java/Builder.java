d2dc438ee3dc025844f42e2a96a350d20bd9b315
amanzhol.temirbolat <Timka-cloud1>
2022-05-09 19:03:35 +0600

    Initial commit

---
+++ src/main/java/Builder.java
@@ -0,0 +1,16 @@
+public interface Builder {
+    User build();
+    Builder name(String name);
+    Builder username(String username);
+    Builder id(Long id);
+    Builder surname(String username);
+    Builder email(String email);
+    Builder phoneNumber(String phoneNumber);
+    Builder password(String password);
+    Builder isActive(boolean active);
+
+
+
+
+
+}
